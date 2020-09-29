aad5c84fa4d07a05770299b39aeea5fb8d3dad49
Parth Sharma <parthsharma151@gmail.com>
2020-09-29 14:43:39 +0530

    add: indiv project vues

---
+++ src/views/scientech.vue
@@ -0,0 +1,137 @@
+<template>
+	<div>
+		<!-- TODO: Get stuff from prismic -->
+		<div class="grid">
+			<!-- test -->
+			<!-- <img src="https://source.unsplash.com/random" alt="" /> -->
+
+			<!-- TODO: FIGURE THIS OUT -->
+			<div id="panel">
+				<router-link to="/">< Go back</router-link>
+
+				<h1>
+					{{ this.fields.name }}
+				</h1>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and
+					typesetting industry. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown
+					printer took a galley of type and scrambled it to make a
+					type specimen book. It has survived not only five centuries,
+					but also the leap into electronic typesetting, remaining
+					essentially unchanged. It was popularised in the 1960s with
+					the release of Letraset sheets containing Lorem Ipsum
+					passages, and more recently with desktop publishing software
+					like Aldus PageMaker including versions of Lorem Ipsum.
+				</p>
+			</div>
+			<div class="imgholder" v-for="index in fields.projects">
+				<img class="img" :src="index.url" alt="" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	Name: "HomeGrid",
+	data() {
+		return {
+			fields: {
+				projects: [],
+				name: [],
+			},
+		};
+	},
+	methods: {
+		// This is an example query, the important part is above.
+		getContent() {
+			this.$prismic.client
+				.query(this.$prismic.Predicates.at("document.type", "project"))
+				.then((document) => {
+					let array = [];
+					var size = Object.keys(document.results[18].data).length;
+					for (let i = 0; i < size; i++) {
+						array.push(Object.values(document.results[18].data)[i]);
+					}
+
+					array.shift();
+					array.shift();
+
+					this.fields.projects = array;
+					console.log(this.fields.projects);
+					this.fields.name = document.results[18].data.title[0].text;
+				});
+		},
+	},
+	created() {
+		this.getContent();
+	},
+};
+</script>
+
+<style scoped>
+a {
+	color: white;
+}
+.grid {
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(18rem, 1fr));
+	grid-auto-rows: 1fr;
+}
+
+h1 {
+	text-align: left;
+	font-size: 1.5rem;
+}
+
+a:hover {
+	color: rgb(173, 173, 173);
+	transition: ease all 0.2s;
+}
+
+.grid::before {
+	content: "";
+	width: 0;
+	padding-bottom: 100%;
+	grid-row: 1 / 1;
+	grid-column: 1 / 1;
+}
+
+.grid > *:first-child {
+	grid-row: 1 / 1;
+	grid-column: 1 / 1;
+}
+
+/* Just to make the grid visible */
+
+.grid > * {
+	background: black;
+	border: 1px white solid;
+}
+
+#panel {
+	padding: 1.5rem 1.5rem;
+	font-family: "AirbnbBook";
+	color: white;
+	overflow: scroll;
+	height: 17.5rem;
+}
+#panel p {
+	overflow: scroll;
+	color: white;
+	font-family: "AirbnbLight";
+}
+
+.imgholder {
+	height: 20.5rem;
+	width: 100%;
+}
+
+.img {
+	height: 20.5rem;
+	width: 100%;
+	/* min-width: 21.5rem; */
+	object-fit: cover;
+}
+</style>
