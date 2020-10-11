79c66a51864eaff9cd9a5019c860e741992d2b4f
Parth Sharma <parthsharma151@gmail.com>
2020-10-11 22:27:48 +0530

    add: bibliography section

--- src/views/Awards.vue
+++ src/views/Awards.vue
@@ -1,5 +1,6 @@
 <template>
-	<main class="grid">
+<div class="hello">
+<main class="grid">
 		<article>
 			<img
 				src="../assets/awards/1. J.K. ARCHITECT OF THE YEAR AWARDS  1995.jpg"
@@ -119,6 +120,72 @@
 			</div>
 		</article>
 	</main>
+	<div class="biblio">
+		<br><br>
+			<h1 style="font-family: AirbnbBold;">Bibliography</h1>
+			<p><strong> Our works have been quite extensively featured in the Indian architectural magazine media. </strong></p>
+			<h2 style="font-family: AirbnbMedium;">Books</h2>
+			<p>
+				<ol>
+					<li><strong>Contemporary Indian Architecture,</strong> an anthology of works by the contemporary generation of Indian architects, by Jagan Shah, Roli Books 2008 pp 104 -115.</li>
+					<li><strong>91 Residences #3 Contemporary Indian Houses</strong> by Incite publications 2011 pp 107-110</li>
+					<li><strong>91 Residences X10ded More contemporary Indian Houses</strong> by Incite publications 2011 pp 71-74.</li>
+					<li><strong>Contemporary Facades, IAG (Interior Architecture Group),</strong> October 2018 “Top Indian Residential Spaces Exteriors + Interiors 2018” pp. 296-305.</li>
+					<li><strong>The Indian Institute of Architects (Award 2018 Shortlisted Projects),</strong> 2018 "IIA Award For Excellence in Architecture 2018" pp 76-78.</li>
+				</ol>
+			</p>
+			<h2 style="font-family: AirbnbMedium;">Magazines and Periodicals</h2>
+			<p>
+				<ol>
+					<li>Indian  Architect & Builder, September 1993. “Young Designers” pp. 98-104.</li>
+					<li>Architecture & Design (Media Transasia) Nov.-Dec. 1996 " Residence" pp. 117-119.</li>
+					<li>Inside Outside,(Business India Group) June 1999 pp."DDA Style in Delhi"pp 96- 101</li>
+					<li>Architecture&Design (MediaTransasia) May-June2000 "A Minimal Vocabulary" Pp 63-66</li>
+					<li>Inside Outside ( Business India Group ) July 2000 "Colonial Nostalgia" pp.60-73</li>
+					<li>Inside Outside ( Business India Group) March 2002," A Designer Dwelling" pp.80-90</li>
+					<li>Inside Outside ( Business India Group) March 2002," Memories of Imphal" pp148-152</li>
+					<li>Indian Design & Interiors(Media Transasia)June- July 2004 Kids room Special. pp.82</li>
+					<li>Indian Design & Interiors(Media Transasia) October 2004 Kitchen Special. pp.36,65.</li>
+					<li>Inside Outside ( Business India Group) January 2005," Seamless Makeover" pp88-97</li>
+					<li>Design Today ( Living Media India Ltd) Feb-March 2005 “ Size It Up” pp. 85-88 + The above also repeated in Good Housekeeping magazine June 2005 pp 84-88</li>
+					<li>Design Today ( Living Media India Ltd) April May2005 “ Insuburbia” pp. 78-87
++       The above also repeated in Good Housekeeping magazine  2005 pp 
+</li>
+					<li>Design Today (Living Media India Ltd) June July 2005 “Space: Above & Beyond”pp.56-70</li>
+					<li>274Design Today (Living Media India Ltd) Oct Nov 2005- 4thAnniversary Issue :Home Design
+“Outdoors” pp.18 – 20
+“ Kids’ Rooms” pp.98 – 100
+“ Recreation Spaces” pp.118 – 119
+“ Kitchens “  pp  138 – 139
+</li>
+					<li>Design Today (Living Media India Ltd) Sept. 2006 “ Green Urban Oyster ”pp. 70- 78</li>
+					<li>Inside Outside ( Business India Group) Oct. 2006," A Burst of Light" pp112-123</li>
+					<li>Architecture&Design (MediaTransasia) Oct. 2006"M House, New Delhi" Pp 94- 102.</li>
+					<li>Indian Design & Interiors(Media Transasia) Feb. 2007 “ Sleek Tones “. Pp 42-46.</li>
+					<li>Society Interiors ( Magna Publishing) July 2007 “ Augmented Spaces “ Pp 70-78.</li>
+					<li>Design Today (Living Media India Ltd) Aug. 2007 “ Light Burst ”pp. 104-110.</li>
+					<li>Inside Outside ( Business India Group) Dec. 2007," Noida Niche" pp 72 – 80.</li>
+					<li>Architecture&Design (MediaTransasia) Dec, 2007 "Playing with Lines" Pp 116-120</li>
+					<li>Design Today (Living Media India Ltd) Jan.. 2008 “ Light Talks ”pp. 26 – 33.</li>
+					<li>Inside Outside ( Business India Group) Sept.2008," The Glass House" pp 136 - 145.</li>
+					<li>Design Today (Living Media India Ltd) Dec.2008 “ Level Play ”pp. 54 – 60.</li>
+					<li>Design Today (Living Media India Ltd) Oct.2009 “ Cool Inside ”pp. 70-76.</li>
+					<li>Design Today (Living Media India Ltd) March.2010 “ Cool Cut ”pp. 28-32.</li>
+					<li>Elle Decor (Ogaan Publications Office) April-May.2010 “ Design for Life ”pp. 186-193.</li>
+					<li>Design Today (Living Media India Ltd) August.2014 “ A Quiet Refuge ”pp. 36-47.</li>
+					<li>Design Matrix (MRJ Group) Jan-Feb.2015 “ A Modern Office ”pp. 60-63.</li>
+					<li>Home Review (Marvel Infomedia) January 2015 “ Organic Haven ”pp. 120-125.</li>
+					<li>Home & Design Trends (Worldwide Media Pvt.Ltd) February 2015 
+“A Lasting Legacy ”pp.102-111
+</li>
+					<li>Home & Design Trends (Worldwide Media Pvt.Ltd) September 2015 
+“India’s Top 10 Homes 2015”pp.76-77- & 86-87
+</li>
+				</ol>
+				& many other features in Outlook, Outlook City Limits, Good Housekeeping, Design Matrix, Home & Design Trends, Home Review, Elle Decore, Society Interior, Inside Outside, & other magazines cover various projects completed by the firm.
+			</p>
+		</div>
+</div>
 </template>
 
 <script>
@@ -130,7 +197,44 @@ export default {
 };
 </script>
 
+<style>
+@font-face {
+	font-family: "AirbnbBlack";
+	src: url(../fonts/AirbnbCereal-Black.ttf) format("truetype");
+}
+
+@font-face {
+	font-family: "AirbnbBold";
+	src: url(../fonts/AirbnbCereal-Bold.ttf) format("truetype");
+}
+
+@font-face {
+	font-family: "AirbnbBook";
+	src: url(../fonts/AirbnbCereal-Book.ttf) format("truetype");
+}
+
+@font-face {
+	font-family: "AirbnbExtraBold";
+	src: url(../fonts/AirbnbCereal-ExtraBold.ttf) format("truetype");
+}
+
+@font-face {
+	font-family: "AirbnbLight";
+	src: url(../fonts/AirbnbCereal-Light.ttf) format("truetype");
+}
+
+@font-face {
+	font-family: "AirbnbMedium";
+	src: url(../fonts/AirbnbCereal-Medium.ttf) format("truetype");
+}
+</style>
+
 <style scoped>
+.biblio{
+	font-family: "AirbnbBook";
+}
+
+
 .grid {
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
